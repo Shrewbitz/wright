25d2d38a4e1a7519cb0181fe928cc493a6f36c7b
Samuel Gieseker <swxg@umich.edu>
2020-04-29 22:34:11 -0400

    add sentence check

--- src/index.js
+++ src/index.js
@@ -9,35 +9,79 @@ import { setCORS } from "google-translate-api-browser";
 document.addEventListener("DOMContentLoaded", () => {
   let drag = dragElement
 
+  //changes hint language
   d3.tsv("/src/data/language.tsv").then((data) => {
     let z = (data.length);
-    // debugger
     for (let i = 0; i < z; i++) {
-      // debugger
-      // const lang = data[i];
       let option = document.createElement("option")
       option.innerHTML = data[i].languageName;
       option.value = data[i].languageCode
       document.getElementById("language").appendChild(option)
     }
-    console.log(z) 
+    // console.log(z) 
   });
 
 
+  let wordArr = []
   
-  
-  d3.tsv("/src/data/sentences.tsv").then((data) => {
+  // waits until promise returns sentence
+  let demo = () => {
+    //checks every half second to see if sentence is correct
+    let interval = setInterval(() => {
+      let section = document.getElementById("words");
+      let wordCollection = section.getElementsByTagName("div");
+      let count = 0
+      let lcheck = []
+      let hcheck = []
+      let length = wordCollection.length
+
+      for (let i = 0; i< length; i++) {
+        lcheck.push(wordCollection[i].offsetLeft)
+        hcheck.push(wordCollection[i].offsetTop)
+      }
+
+      for (let i = 1; i < lcheck.length; i++) {
+        let left =   (lcheck[i] - lcheck[i - 1]);
+        (( left < 300) && (left > 50)) ? count += 1 : count; 
+        let height = (hcheck[i] - hcheck[i - 1]); 
+        (( height < 100) && (height > -100)) ? count += 1 : count;   
+      }
+
+      //makes progress bar fill up
+      let progress = document.getElementById("progress");
+      progress.max = (length - 1)*2
+      progress.value = count
+
+      //win condition
+      if (count === (length - 1)*2) {
+        clearInterval(interval)
+        console.log("you did it!!!!")
+        document.getElementById("win").style.borderColor = "#1ef325"
+        // setTimeout(() => {document.getElementById("win").addEventListener("click", location.reload());}, 1000);
+        setTimeout(() => {location.reload()}, 3000);
+      } else { ////////
+        // console.log("not yet")
+        // console.log(count)
+      }  //////////
+
+    }, 500)
+
+  }
+  setTimeout(demo, 1000);
+
+
+  //picks a rando sentence
+ let startUp = d3.tsv("/src/data/sentences.tsv").then((data) => {
     let x = (data[Math.floor(Math.random() * 20000)].sentence);
     console.log(x);
     
-    
-    
     //makes dropdown and also changes language of hint
-    document.getElementById("language").addEventListener('change',
-      (event) => {
-        let langugePicker = event.target.value;
-        const translate = setCORS("http://cors-anywhere.herokuapp.com/");
-        translate(x, { to: langugePicker })
+    
+    let lang = localStorage.getItem('myLanguage')
+    if (lang && (lang != " ")) {
+      // debugger
+      const translate = setCORS("http://cors-anywhere.herokuapp.com/");
+        translate(x, { to: lang })
         .then(res => {
           document.getElementById("translation").innerHTML = (res.text)
           console.log(res.text)
@@ -45,14 +89,35 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(err => {
           console.error(err);
         });
+    } 
+    //lets user change language
+    document.getElementById("language").addEventListener('change',
+      (event) => {
+        let langPicker = event.target.value;
+        //lets you change back to no hint
+        if (langPicker === ' ') {
+          document.getElementById("translation").innerHTML = ("Choose a language for your hint.")
+          localStorage.setItem('myLanguage', ` `);
+        } else {
+          localStorage.setItem('myLanguage', `${langPicker}`);
+          const translate = setCORS("http://cors-anywhere.herokuapp.com/");
+          translate(x, { to: langPicker })
+          .then(res => {
+            document.getElementById("translation").innerHTML = (res.text)
+            console.log(res.text)
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        } 
       }
     );
         
       // places words
-      let word_arr = x.split(" ")
-      for (let i = 0; i < word_arr.length; i++) {
+      wordArr = x.split(" ")
+      for (let i = 0; i < wordArr.length; i++) {
         let word = document.createElement("div")
-        word.innerHTML = word_arr[i];
+        word.innerHTML = wordArr[i];
         word.style.top=`${Math.random() * 520 }px`;
         word.style.left=`${Math.random() * 1050 + 10}px`;
         document.getElementById("words").appendChild(word)
--- src/scripts/words.js
+++ src/scripts/words.js
@@ -20,7 +20,13 @@
         pos3 = e.clientX;
         pos4 = e.clientY;
         ele.style.top = (ele.offsetTop - pos2) + "px";
+        if (ele.offsetTop > 540) {ele.style.top = "540px"}
+        if (ele.offsetTop < -26) {ele.style.top = "-26px"}
+        // debugger
         ele.style.left = (ele.offsetLeft - pos1) + "px";
+        // debugger
+        if (ele.offsetLeft + (ele.innerText.length * 12) > 1188) {ele.style.left = `${1188 - (ele.innerText.length * 12)}px`}
+        if (ele.offsetLeft < -3) {ele.style.left = "-3px"}
       }
     
       function closeDragElement() {
